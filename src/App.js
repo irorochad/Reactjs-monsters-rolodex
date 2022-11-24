353b474430b902302500f8908af01f1e098e9762
irorochad <wisdomchadere@gmail.com>
2022-11-24 23:35:06 +0100

    created and added components to the app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { Component } from "react";
 
 // import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list.components";
 import "./App.css";
 
 class App extends Component {
   constructor() {
+    console.log("constructor");
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
   componentDidMount() {
+    console.log("ComponentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -26,15 +30,36 @@ class App extends Component {
       );
   }
   render() {
+    console.log("render");
+    const onchangeBtn = (event) => {
+      const searchField = event.target.value.toLocaleLowerCase();
+
+      this.setState(() => {
+        return { searchField };
+      });
+    };
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search"
+          onChange={onchangeBtn}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
+            
           );
         })}
+        <CardList />
       </div>
     );
   }
---
+++ src/components/card-list/card-list.components.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return  <h1>Hi, I'm from the CardList Component</h1>;
+   
+  }
+}
+
+export default CardList;
