e2fbb873c59d517c2eb342eebc05fa109a8acb3d
irorochad <wisdomchadere@gmail.com>
2022-11-25 18:24:03 +0100

    added styles to the app

--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -1,13 +1,19 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
+          <div key={monster.id} className="card-container">
+            <img
+              src={`https://robohash.org/${monster.id}?set=set2`}
+              alt="Monster Image"
+            />
+            <h2>{monster.name}</h2>
+            <p>{monster.email}</p>
           </div>
         ))}
       </div>
